0978e599c440aaaeedfa94e8cc6f749f7c5c2aa4
Martin Perren <martinperren@gmail.com>
2019-02-19 19:03:31 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -536,7 +536,7 @@ message.channel.send(`__**BOT UPTIME:**__ ${days} DIAS ${hrs} HS ${mins} MINS`);
 		if(link.length!=0){
 		
 		const voiceChannel = message.member.voiceChannel;
-				var video = await youtube.getVideo(link);
+				var video = await youtube.getVideo(String(link));
 				var playlist = false;
 				handleVideo(video, message, voiceChannel, playlist);
 				//message.channel.send('!cc 2');
