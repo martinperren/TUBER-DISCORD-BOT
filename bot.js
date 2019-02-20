60857e89a9b5c5e2816eb5e2e16835bce50dec0b
MARTIN <martinperren@gmail.com>
2019-02-20 17:14:27 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ var db = mysql.createConnection({
     user: process.env.sqluser,
     password: process.env.sqlpass,
     database: process.env.sqldb,
-    charset: "latin1_swedish_ci"
+    charset: "utf8mb4"
 });
 
 
