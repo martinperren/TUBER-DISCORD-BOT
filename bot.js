f39319732b2c27640c643cc79ebf366caff92fec
Martin Perren <martinperren@gmail.com>
2019-11-20 13:09:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,8 +60,8 @@ client.on("guildDelete", guild => {
  client.user.setGame(process.env.GAME);		
 });
 client.on('guildMemberAdd', member => {
-    //member.guild.channels.get('555046804807221248').send('**' + member.user.username + '** ahora vive en MAIAMEEEEE! :house:');
-	member.user.send("test.")
+    member.guild.channels.get('555046804807221248').send('**' + member.user.username + '** ahora vive en MAIAMEEEEE! :house:');
+	member.user.send("Bienvenido al búnker, comeme los huevos.")
 	
 member.addRole('537712377634881545');
 });
