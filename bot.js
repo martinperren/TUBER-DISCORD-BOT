a4662a6275f92575ca2479d979e3659cfcfba1df
Martin Perren <martinperren@gmail.com>
2020-04-24 12:58:28 -0300

    added reminder

--- bot.js
+++ bot.js
@@ -5,9 +5,15 @@ const restClient = new Client();
 //var vel = 4320000000;
 var admin = ["Owner", "Admin", "Bunker Support"];
 var roles = ["Owner", "Admin", "Bunker Support","Mods"];
-
+var mysql = require('mysql');
 
 //  START  //  START  //  START  //  START  //  START  //  START  //  START  //  START  //  START  //  START  //  START  
+
+
+
+var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+
+
 
 
 
@@ -208,6 +214,140 @@ client.on("message", async message => {
 */
 
 
+if (msg.content.toLowerCase().startsWith('!recme')) {
+        var message = msg;
+        try {
+            
+            // Variables
+            var returntime;
+            var timemeasure;
+            msg = msg.content.split(' ');
+            console.log('Mensaje recibido de ' + message.author.id + ' a las ' + Date.now().toString());
+
+            // Sets the return time
+            timemeasure = msg[1].substring((msg[1].length - 1), (msg[1].length))
+            returntime = msg[1].substring(0, (msg[1].length - 1))
+
+            // Based off the delimiter, sets the time
+            switch (timemeasure) {
+                case 's':
+                    returntime = returntime * 1000;
+                    break;
+
+                case 'm':
+                    returntime = returntime * 1000 * 60;
+                    break;
+
+                case 'h':
+                    returntime = returntime * 1000 * 60 * 60;
+                    break;
+
+                case 'd':
+                    returntime = returntime * 1000 * 60 * 60 * 24;
+                    break;
+
+                default:
+                    returntime = returntime * 1000;
+                    break;
+            }
+
+            // Returns the Message
+            client.setTimeout(function () {
+                // Removes the first 2 array items
+                msg.shift();
+                msg.shift();
+
+                // Creates the message
+                var content = msg.join();
+                content = content.replaceAll(',', ' ');
+                message.reply(content);
+                console.log('Mensaje enviado a ' + userid + ' a las ' + Date.now().toString());
+            }, returntime)
+        } catch (e) {
+           message.reply("Error, asegurate de ingresar un mensaje y el tiempo.");
+            console.error(e.toString());
+        }
+
+    // List of commands
+    }else if (msg.content.toLowerCase() === "!reminderbot") {
+        msg.channel.send("Hello I am reminder bot:\n\n!reminderbot \t\tList of all Commands\n!quit \t\tTurns off the bot\n!remindme \t\t {time} {message}\n\t{time} Please have the amount of time be denoted by a time character.\n\t\tm - minutes, s - seconds, d - days.\n!remind {@User} {time} {message}\n\t{time} Please have the amount of time be denoted by a time character.\n\t\tm - minutes, s - seconds, d - days.\n\t{@User} So far you can use the user's name with the @ symbol.\n\n--- Created and Managed by pixlbreaker ---");
+    
+    // Reminds a specific user
+    } else if (msg.content.toLowerCase().startsWith('!rec')) {
+        var message = msg;
+        try {
+            
+            // Variables
+            var returntime;
+            var timemeasure;
+            var userid;
+            msg = msg.content.split(' ');
+            console.log('Mensaje recibido de ' + message.author.id + ' a las ' + Date.now().toString());
+
+            // Sets the userid for the recipiant
+            userid = client.users.get(msg[1].replace('<@!', '').slice(0, -1))
+            
+            // Sets the return time
+            timemeasure = msg[2].substring((msg[2].length - 1), (msg[2].length))
+            returntime = msg[2].substring(0, (msg[2].length - 1))
+
+            // Based off the delimiter, sets the time
+            switch (timemeasure) {
+                case 's':
+                    returntime = returntime * 1000;
+                    break;
+
+                case 'm':
+                    returntime = returntime * 1000 * 60;
+                    break;
+
+                case 'h':
+                    returntime = returntime * 1000 * 60 * 60;
+                    break;
+
+                case 'd':
+                    returntime = returntime * 1000 * 60 * 60 * 24;
+                    break;
+
+                default:
+                    returntime = returntime * 1000;
+                    break;
+            }
+
+            // Returns the Message
+            client.setTimeout(function () {
+                // Removes the first 2 array items
+                msg.shift();
+                msg.shift();
+                msg.shift();
+
+                // Creates the message
+                var content = msg.join();
+                content = content.replaceAll(',', ' ');
+                message.channel.send(userid + content);
+                console.log('Mensaje enviado a ' + userid + ' a las ' + Date.now().toString());
+            }, returntime)
+        } catch (e) {
+            message.reply("Error, asegurate de ingresar un mensaje y el tiempo.");
+            console.error(e.toString());
+        }
+
+    // List of Commands
+    } 
+
+
+       
+
+
+
+
+
+
+
+      
+    
+
+
 	if (message.content.includes("huevo")) {
 		message.react("537716624296378399");
 	}
@@ -218,8 +358,6 @@ client.on("message", async message => {
 		const ayy = client.emojis.cache.get("537716624296378399");
 		message.channel.send(`¿y el ${ayy}?`);
 	}
-
-
 
 
 	if (message.content.startsWith("!say")){
